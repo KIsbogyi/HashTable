3a1fa2080cb66d22c846433adfd7f38adb207e5a
Kisbogyi <55801726+KIsbogyi@users.noreply.github.com>
2021-11-27 12:14:00 +0100

    Add files via upload

--- jsonloader.c
+++ jsonloader.c
@@ -1,66 +1,123 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void loader(void){
-	FILE *fp = fopen("test.json", "rt");
-	char stream[400];
+#include "debugmalloc.h"
+#include "jsonloader.h"
+
+
+int loader(sentinel **lista,char *name){
+	strcat(name, ".json");
+	FILE *fp = fopen(name, "rt");
+	if(fp == NULL){
+		return 1;
+	}
+	else{
+	int size = 2;						//first char + \0
+	char *stream = (char *)malloc(size * sizeof(char));
+	if(stream == NULL){
+		return 2;
+	}
+	sprintf(stream, "%s", "");
 
 	char c = fgetc(fp);
 	if (c != '{'){
-		stream[0] = c;
-		stream[0+1] = '\0';	
+		return 1;
 	}
 	while (1){
 		c = fgetc(fp);
 		if (feof(fp)){
 			break;
 		}
-		if (c != '{' && c != '}' && c != '"' && c != ' '){ 
-			stream[strlen(stream)] = c;
-			stream[strlen(stream)+1] = '\0';
+		if (c != '{' && c != '}' && c != '"' && c != ' ' && c != '\n'){
+			size ++;
+			stream = (char *)realloc(stream, size * sizeof(char));
+			if(stream == NULL){
+				return 2;
+			}
+			int len = strlen(stream);
+			stream[len] = c;
+			stream[len+1] = '\0';
 		}
 	}
 
-
-	char *data[100];
+	char **data = (char **)malloc(1 * sizeof(char*));
+	if(data == NULL){
+		return 2;
+	}
 	int counter = 0;
 	char *token = strtok(stream, ",");
 
 	while(token != NULL){
+		data = (char **)realloc(data, (counter+1)*sizeof(char*));
+		if(data == NULL){
+			return 2;
+		}
 		data[counter] = token;
 		counter ++;
 		token = strtok(NULL, ",");
-		
+
 	}
-
-
+	
 	for(int i = 0; i < counter; i++){
 		char *touple = strtok(data[i], ":");
 		int helper = 0;
 		while (touple != NULL){
 			if (helper % 2 == 0 ){
-				printf("%s ", touple);
 			}
 			else{
-				printf("%s\n", touple);
+				char *temp = (char *)malloc(sizeof(char)*strlen(touple)+1);
+				strcpy(temp, touple);
+				adder(lista, temp);
+
 			}
 			helper ++;
 			touple = strtok(NULL, ":");
 		}
 	}
+	free(stream);
+	free(data);
 	fclose(fp);
-}
-
-void writer(char *file, char *data){
-	FILE *fp = fopen(file, "wt");
-	fprintf(fp, "{%s}", data);
-	fclose(fp);
-	
-}
-
-
-int main(void){
-//	loader();
-	writer("alma.json", "\"s\": \"schiman\"");
+	}
 	return 0;
+
 }
+
+int fwriter(char *file, sentinel **lista, int size){
+	char *word = (char *)malloc(sizeof(char)*2);
+	if(word == NULL){
+		return 2;
+	}
+	sprintf(word, "%s", "{");
+	for(int i = 0; i < size; i++){
+		Node *iter = lista[i]->first->next;
+		while(iter != lista[i]->last){
+			char *tmp = (char *)malloc(sizeof(char)*(strlen(iter->data)+10));
+			if(tmp == NULL){
+				return 2;
+			}
+			sprintf(tmp, "\"%c\": \"%s\", ",iter->data[0], iter->data);
+			word = (char *)realloc(word, sizeof(char)*(strlen(word)+1+strlen(tmp)));
+			if(word == NULL){
+				return 2;
+			}
+			strcat(word, tmp);
+			iter = iter->next;
+			free(tmp);
+		}
+	}
+
+	word[strlen(word)-2] = '}';
+	strcat(file, ".json");
+	FILE *fp = fopen(file, "wt");
+	if(fp == NULL){
+		return 1;
+	}
+	fprintf(fp, "%s", word);
+	free(word);
+	fclose(fp);
+	return 0;
+
+}
+
+
